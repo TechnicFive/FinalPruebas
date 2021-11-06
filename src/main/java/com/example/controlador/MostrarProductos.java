118e178a54fa48bfd56d3fd8ab4ad87e5b6f0056
TechnicFive <flufi@usal.es>
2021-11-06 13:15:54 +0100

    Logearse

--- src/main/java/com/example/controlador/MostrarProductos.java
+++ src/main/java/com/example/controlador/MostrarProductos.java
@@ -1,11 +1,17 @@
 package com.example.controlador;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.modelos.Usuarios;
+import com.example.modelos.Productos;
 import com.example.servicios.ProductoServicios;
 
 
@@ -16,8 +22,18 @@ public class MostrarProductos {
 	ProductoServicios usu;
 	
 	@GetMapping("")
-	public String Productos(Model model) {
+	public String Productos(Model model, HttpSession sesion) {
+		crearCarrito(sesion);
 		model.addAttribute("Productos", usu.DarProducto());
 		return "index";
+	}
+
+	
+	public void crearCarrito(HttpSession sesion) {
+		ArrayList <Productos> lista= new ArrayList <Productos>();
+		
+		sesion.setAttribute("carrito", lista);
+		//Cuando compremos se pone null
+			
 	}
 }
--- src/main/java/com/example/controlador/RegistroControlador.java
+++ src/main/java/com/example/controlador/RegistroControlador.java
@@ -1,18 +1,29 @@
 package com.example.controlador;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.modelos.Usuarios;
 import com.example.servicios.UsuarioServicios;
+
+
 
 @Controller
 @RequestMapping("/usuarios")
 public class RegistroControlador {
 	@Autowired
 	UsuarioServicios usu;
+	
 	
 	@GetMapping("/base")
 	public void base() {
@@ -24,6 +35,25 @@ public class RegistroControlador {
 		
 		return "loginForm";
 	}
+	
+	@PostMapping("/login")
+	public String login(@ModelAttribute Usuarios usuario, Model model, HttpSession sesion, RedirectAttributes redirect) {
+		System.out.println("Email: " + usuario.getEmail() + " Clave: " + usuario.getClave());
+		Usuarios u=usu.findByEmail(usuario.getEmail());
+		System.out.println("probando");
+		//String pagina="redirect:/usuarios/registro";
+		if(u!=null) {
+			if(u.getClave().equals(usuario.getClave())) {
+				System.out.println("probando");
+				sesion.setAttribute("usuario", u);
+				return "holaUsuario";
+			}else return "redirect:/usuarios/registro";
+			
+		}else return "redirect:/usuarios/registro";
+		
+		
+	}
+	
 	
 	
 }
